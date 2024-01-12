056625905a92a9e942252488663656a9ec6fc54b
victorchukwuemeka <chukwuemekavictor693@gmail.com>
2024-01-12 02:19:33 +0100

    latest

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -27,7 +27,7 @@ class ListingController extends Controller
     public function store(Request $request){
 
 
-    //dd($request);
+    dd($request);
     Listing::validate($request);
     $listing  = new Listing();
     $user_id = $user_id_in_session = Auth::id();
